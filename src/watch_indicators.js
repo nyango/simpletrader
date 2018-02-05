cbadf61d5d9bcfa7efbf8fc3e1cae78017e1771d
nyango <nyango.tech@gmail.com>
2018-02-05 15:39:04 +0900

    パラメタ調整/util.printはdeprecatedなのでやめる

--- src/watch_indicators.js
+++ src/watch_indicators.js
@@ -2,7 +2,6 @@ const fetch = require("node-fetch");
 const ti = require('technicalindicators');
 const ccxt = require('ccxt');
 const https = require('https');
-const util = require('util');
 
 rangeMin = 260;
 async function getAsJson(url) {
@@ -22,11 +21,11 @@ function sleep(time) {
 
 const macdInput = {
   values : null,
-  fastPeriod : 5,
-  slowPeriod : 8,
-  signalPeriod : 3,
+  fastPeriod : 50,
+  slowPeriod : 80,
+  signalPeriod : 30,
   SimpleMAOscillator: false,
-  SimpleMASignal: true
+  SimpleMASignal: false
 };
 
 const MACD = ti.MACD;
@@ -63,25 +62,23 @@ const patternMap = {
     series = series.slice(-rangeMin);
 
     // show indicators
-    const pattern = await ti.predictPattern({ values : series })
 
     macdInput.values = series;
     const macd = MACD.calculate(macdInput);
-    const macdSecond = macd.slice(-2)[0];
     const macdLatest = macd.slice(-1)[0];
-
-    const sma = ti.SMA.calculate({period : 3, values : series});
-    const bb = BB.calculate({period : 3, values : series, stdDev: 2});
+    const sma = ti.SMA.calculate({period : 10, values : series});
+    const bb = BB.calculate({period : 60, values : series, stdDev: 2});
     const bbLatest = bb.slice(-1)[0];
     // console.log(ccxt.omit (ticker, 'info'));
     // 必要そうな情報をLTSVで出力
-    util.print(`bid:${ticker.bid}\task:${ticker.ask}\tlast:${ticker.last}\ttime:${ticker.datetime}\tvolume:${ticker.baseVolume}`)
-    util.print(`\tpattern:${patternMap[pattern.pattern]}\tpatternId:${pattern.patternId}\tprob:${pattern.probability}`);
+    process.stdout.write(`bid:${ticker.bid}\task:${ticker.ask}\tlast:${ticker.last}\ttime:${ticker.datetime}\tvolume:${ticker.baseVolume}`)
     if (macdLatest) {
-      util.print(`\tmacd:${macdLatest.MACD}\thist:${macdLatest.histogram}\tsignal:${macdLatest.signal}`);
+      const pattern = await ti.predictPattern({ values : macd.map((v) => v.MACD) })
+      process.stdout.write(`\tpattern:${patternMap[pattern.pattern]}\tpatternId:${pattern.patternId}\tprob:${pattern.probability}`);
+      process.stdout.write(`\thist:${macdLatest.histogram}\tmacd:${macdLatest.MACD}\tsignal:${macdLatest.signal}`);
     }
     if (bbLatest) {
-      util.print(`\tbbLower:${bbLatest.lower}\tbbMiddle:${bbLatest.middle}\tbbUpper:${bbLatest.upper}`);
+      process.stdout.write(`\tbbLower:${bbLatest.lower}\tbbMiddle:${bbLatest.middle}\tbbUpper:${bbLatest.upper}`);
     }
     console.log(`\tsma:${sma.slice(-1)[0]}`);
   }
